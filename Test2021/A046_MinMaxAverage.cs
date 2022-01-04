f957b2042ea91ac74bcb8dc606f083fc8df9ed25
hyo jin park <qkrgywls332@gmail.com>
2022-01-04 11:25:06 +0900

    220103 평균, 최소, 최대값 구하기 commit

---
+++ Test2021/A046_MinMaxAverage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2021
+{
+    class A046_MinMaxAverage
+    {
+        /*평균, 최소, 최댓값 구하기*/
+
+        static void Main(string[] args)
+        {
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            double sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("키를 입력하세요(단위: cm) : ");
+                double h = double.Parse(Console.ReadLine());
+                if (h > max)
+                {
+                    max = h;
+                }
+                if (h < min)
+                {
+                    min = h;
+                }
+                sum += h;
+            }
+
+            Console.WriteLine("평균 : {0}, 최소 : {1}, 최대 : {2}", sum/5, min, max);
+        }
+    }
+}
