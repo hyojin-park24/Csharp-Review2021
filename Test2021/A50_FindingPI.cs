23dcf66ae16f289698aed7a50ac219ce1e7b8205
hyo jin park <qkrgywls332@gmail.com>
2022-01-10 09:53:33 +0900

    220110 라이프니츠의 원주율 공식 계산 commit

---
+++ Test2021/A50_FindingPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2021
+{
+    class A50_FindingPI
+    {
+        /*라이프니츠의 원주율 공식 계산*/
+
+        static void Main(string[] args)
+        {
+            bool sign = false;  //한번은 빼기, 한번은 더하기 위해 bool타입 sign변수에 false대입
+            double pi = 0;
+
+            for (int i = 0; i <= 10000; i += 2) //i는 1~10000까지 2씩 증가하면서 반복
+            {
+                if (sign == false)  //sign이 false이면 덧셈
+                {
+                    pi += 1.0 / i;  
+                    sign = true;
+                }
+                else
+                {
+                    pi -= 1.0 / i;  //sign이 true면 뺄셈
+                    sign = false;
+                }
+                Console.WriteLine("i = {0}, PI = {1}", i, 4*pi);
+            }
+        }
+    }
+}
